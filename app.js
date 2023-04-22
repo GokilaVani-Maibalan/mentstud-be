80de4577f60c582ce867a91cc13eb1f5bea14241
GokilaVani-Maibalan <gokilamaibalan64@gmail.com>
2023-04-22 10:44:40 +0530

    "first commit"

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ var mentorStudentRouter = require("./routes/mentorStudent");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
